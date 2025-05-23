9d7a4d048a70688f9bfa222bc2ba0415e03ca07a
gitamadoudiene <amadoudiene20@gmail.com>
2025-05-23 01:41:50 +0000

    third

--- routes/api.js
+++ routes/api.js
@@ -55,39 +55,37 @@ module.exports = function (app) {
   if (!body._id) {
     return res.json({ error: 'missing _id' });
   }
+
   if (!issues[project]) issues[project] = [];
+
   let issue = issues[project].find(i => i._id === body._id);
+
   if (!issue) {
+    // _id trouvé mais pas d'issue correspondante
     return res.json({ error: 'could not update', _id: body._id });
   }
 
-  // Ne pas supprimer _id, sinon il disparait dans la réponse plus bas
-  // delete body._id;  <-- SUPPRIME cette ligne
+  // On exclut _id et on regarde s'il y a des champs à mettre à jour
+  const updatableFields = ['issue_title', 'issue_text', 'created_by', 'assigned_to', 'status_text', 'open'];
+  const fieldsToUpdate = updatableFields.filter(field => body[field] !== undefined && body[field] !== '');
 
-  const updateFields = Object.keys(body).filter(key => body[key] !== undefined && body[key] !== '' && key !== '_id');
-
-  if (updateFields.length === 0) {
+  if (fieldsToUpdate.length === 0) {
     return res.json({ error: 'no update field(s) sent', _id: body._id });
   }
 
-  try {
-    updateFields.forEach(field => {
-      if (field === 'open') {
-        issue.open = body.open === 'false' ? false : Boolean(body.open);
-      } else {
-        issue[field] = body[field];
-      }
-    });
+  // Met à jour les champs
+  fieldsToUpdate.forEach(field => {
+    if (field === 'open') {
+      issue.open = body.open === 'false' ? false : Boolean(body.open);
+    } else {
+      issue[field] = body[field];
+    }
+  });
 
-    issue.updated_on = new Date();
-    return res.json({ result: 'successfully updated', _id: body._id });
-  } catch (err) {
-    return res.json({ error: 'could not update', _id: body._id });
-  }
-    })
+  issue.updated_on = new Date();
 
-
-    
+  return res.json({ result: 'successfully updated', _id: body._id });
+})
     .delete((req, res) => {
       let project = req.params.project;
       let body = req.body;
